5da6e4c63109d3049ab28d7e54e40ef44e034389
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-06-24 18:15:15 +0800

    fix: add OpenAI key exisitance check

--- src/generate.ts
+++ src/generate.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import Mustache from 'mustache';
-import type { Editor } from 'obsidian';
+import { type Editor, Notice } from 'obsidian';
 
 import { callAPI } from './ai';
 import { log } from './logging';
@@ -9,23 +9,24 @@ import type { CommandNames } from './prompts';
 import { PROMPTS } from './prompts';
 import type { PluginSettings } from './types';
 
-export async function runPrompts(
-	editor: Editor,
-	settings: PluginSettings,
-	command: CommandNames,
-) {
+export async function runPrompts(editor: Editor, settings: PluginSettings, command: CommandNames) {
+	if (!settings.openAiApiKey || settings.openAiApiKey === '') {
+		new Notice('No OpenAI API key set');
+		return;
+	}
+
 	log(settings, `Running prompt with command ${command}`);
 
-	const sel = editor.getSelection();
+	const input = editor.getSelection();
 
-	if (sel.length > 0) {
+	if (input.length > 0) {
 		// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 		const promptData = PROMPTS.find(p => p.name === command);
 
 		if (!promptData) throw new Error(`Could not find prompt data with name ${command}`);
 
 		const prompt: string = Mustache.render(promptData.description, {
-			input: sel,
+			input,
 		});
 
 		const result = await callAPI(settings, prompt);
