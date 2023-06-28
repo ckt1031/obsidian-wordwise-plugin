e99504ab22dac7d5a9b860d9dfa61871a5332d2a
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-06-28 23:56:24 +0800

    fix: svg icons 2

--- src/icons/fix-grammar.ts
+++ src/icons/fix-grammar.ts
@@ -1,4 +1,4 @@
 const icon =
-	'<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>';
+	'<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>';
 
 export default icon;
--- src/icons/make-longer.ts
+++ src/icons/make-longer.ts
@@ -1,4 +1,4 @@
 const icon =
-	'<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M14 17H4v2h10v-2zm6-8H4v2h16V9zM4 15h16v-2H4v2zM4 5v2h16V5H4z"></path></svg>';
+	'<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M14 17H4v2h10v-2zm6-8H4v2h16V9zM4 15h16v-2H4v2zM4 5v2h16V5H4z"/></svg>';
 
 export default icon;
