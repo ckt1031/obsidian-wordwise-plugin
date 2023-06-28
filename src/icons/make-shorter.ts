80bf21f71177014341889f038b8ef89dc471cb79
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-06-28 23:34:20 +0800

    fix: svg icons

--- src/icons/make-shorter.ts
+++ src/icons/make-shorter.ts
@@ -1,4 +1,4 @@
-const icon = `<svg viewBox="0 0 24 24">
+const icon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
 <path d="M4 9h16v2H4V9zm0 4h10v2H4v-2z"></path>
 </svg>`;
 
