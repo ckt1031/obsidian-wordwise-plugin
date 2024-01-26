f243761a724804341ce2eeb57b15495369c6ac4d
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-01-26 15:02:06 +0800

    Update OpenAI models in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,19 @@
 import type { PluginSettings } from './types';
 
+// Ref: https://platform.openai.com/docs/models/overview
+// Updated on 2024-01-26
 export const OPENAI_MODELS = [
 	'gpt-3.5-turbo',
 	'gpt-3.5-turbo-1106',
+	// 'gpt-3.5-turbo-0125', // This model will be released soon
 	'gpt-3.5-turbo-16k',
 	'gpt-4',
-	'gpt-4-32k',
 	'gpt-4-0613',
+	'gpt-4-32k',
 	'gpt-4-32k-0613',
+	'gpt-4-turbo-preview',
 	'gpt-4-1106-preview',
-	'gpt-4-vision-preview',
+	'gpt-4-0125-preview',
 ];
 
 export const DEFAULT_API_HOST = 'https://api.openai.com';
