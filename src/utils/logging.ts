40d7b3bd8939e59b1d26ccbdabdc5c6c7f53896f
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-02-12 06:19:39 +0000

    fix: logging

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -2,5 +2,5 @@ import type { PluginSettings } from '@/types';
 import manifest from '../../manifest.json';
 
 export function log(settings: PluginSettings, message: string) {
-	if (settings.debugMode) console.log(`${manifest.name}:`, message);
+	if (settings?.debugMode) console.log(`${manifest.name}:`, message);
 }
